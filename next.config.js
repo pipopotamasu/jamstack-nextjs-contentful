cd9c586e742880ef114a9f4246020315f5d831a0
pipopotamasu <ec0156hx@gmail.com>
2020-12-05 21:08:20 +0900

    preview api

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
 
+console.log(process.env.NODE_ENV);
+
+const isProduction = process.env.NODE_ENV === 'production' ;
+
 module.exports = {
   basePath: process.env.GITHUB_PAGES ? '/jamstack-nextjs-contentful' : '',
   assetPrefix: process.env.GITHUB_PAGES ? '/jamstack-nextjs-contentful' : '',
   env: {
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
-    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN
+    CONTENTFUL_ACCESS_TOKEN: isProduction ? process.env.CONTENTFUL_ACCESS_TOKEN : process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
+    CONTENTFUL_HOST: isProduction ? 'api.contentful.com' : 'preview.contentful.com'
   }
 };
