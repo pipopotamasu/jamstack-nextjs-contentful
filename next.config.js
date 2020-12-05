475406c324c1c1258f605dcebcb365a51c4e7c2d
pipopotamasu <ec0156hx@gmail.com>
2020-12-05 21:22:08 +0900

    fix host (#23)

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 
-console.log(process.env.NODE_ENV);
-
-const isProduction = process.env.NODE_ENV === 'production' ;
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
   basePath: process.env.GITHUB_PAGES ? '/jamstack-nextjs-contentful' : '',
@@ -10,6 +8,6 @@ module.exports = {
   env: {
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
     CONTENTFUL_ACCESS_TOKEN: isProduction ? process.env.CONTENTFUL_ACCESS_TOKEN : process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
-    CONTENTFUL_HOST: isProduction ? 'api.contentful.com' : 'preview.contentful.com'
+    CONTENTFUL_HOST: isProduction ? 'cdn.contentful.com' : 'preview.contentful.com'
   }
 };
